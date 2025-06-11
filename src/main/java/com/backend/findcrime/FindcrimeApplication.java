a23235a95d60bddaa705bd699c2354400ac07385
mmije0ng <victon777@naver.com>
2025-06-11 19:08:36 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/backend/findcrime/FindcrimeApplication.java
@@ -0,0 +1,13 @@
+package com.backend.findcrime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindcrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindcrimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/findcrime/FindcrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.findcrime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindcrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
